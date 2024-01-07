d5330b09534c6dfdb7c0aa2da5bbdc112a7b01f8
JosiasAldaz <josiasjoestar@hotmail.com>
2024-01-06 23:16:00 -0500

    Initial commit

---
+++ src/main/java/com/example/segundo_deber/BackendYourKeysApplication.java
@@ -0,0 +1,13 @@
+package com.example.segundo_deber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendYourKeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendYourKeysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/segundo_deber/BackendYourKeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.segundo_deber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendYourKeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
