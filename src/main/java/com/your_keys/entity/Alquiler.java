1f931c53cca41ab3f9f1953e7da75fef6e88a23d
JosiasAldaz <josiasjoestar@hotmail.com>
2024-01-07 00:34:06 -0500

    tabla auto y alquiler

--- src/main/java/com/your_keys/entity/Alquiler.java
+++ src/main/java/com/your_keys/entity/Alquiler.java
@@ -1,5 +1,32 @@
 package com.your_keys.entity;
 
-public class Alquiler {
+import java.io.Serializable;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="alquiler")
+public class Alquiler implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id_alquiler;
+	private Long id_cliente;
+
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="id_alquiler")
+	private List<Auto> autos;
 }
--- src/main/java/com/your_keys/entity/Cliente.java
+++ src/main/java/com/your_keys/entity/Cliente.java
@@ -1,5 +1,63 @@
 package com.your_keys.entity;
 
-public class Cliente {
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.io.Serializable;
+import java.util.List;
 
+
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id_cliente;
+	private Long id_persona;
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="id_cliente")
+	private List<Alquiler> alquiler;
+
+	public Long getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(Long id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public Long getId_persona() {
+		return id_persona;
+	}
+
+	public void setId_persona(Long id_persona) {
+		this.id_persona = id_persona;
+	}
+
+	public List<Alquiler> getAlquiler() {
+		return alquiler;
+	}
+
+	public void setAlquiler(List<Alquiler> alquiler) {
+		this.alquiler = alquiler;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 }
