a34ae2c172a7478a49c64a1c9586458722418258
DavidGuambana <112259062+DavidGuambana@users.noreply.github.com>
2024-01-17 07:46:19 -0500

    CrossOrigin
    CrossOrigin

--- src/main/java/com/your_keys/controllers/ctrlRol.java
+++ src/main/java/com/your_keys/controllers/ctrlRol.java
@@ -3,6 +3,7 @@ package com.your_keys.controllers;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.your_keys.entity.Rol;
 import com.your_keys.services.serRol;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ctrlRol {
